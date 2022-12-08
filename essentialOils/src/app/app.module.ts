de38b6861ac58a4fbe099ce126ef79df6a5ee709
Matt Van Harn <mattvanharn@gmail.com>
2022-12-07 19:57:58 -0500

    Create components, create service for mock data

--- essentialOils/src/app/app.module.ts
+++ essentialOils/src/app/app.module.ts
@@ -2,13 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { DisplayProductComponent } from './display-product/display-product.component';
+import { EditProductComponent } from './edit-product/edit-product.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddProductComponent,
+    DisplayProductComponent,
+    EditProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ essentialOils/src/app/mock-products.ts
@@ -0,0 +1,7 @@
+import { Product } from "./product";
+
+export const PRODUCTS: Product[] = [
+  { name: "Essential Oil", description: "This oil totally does something.", uses: [true, false], benefits: [false, true] },
+  { name: "Super Essential Oil", description: "Not a scam", uses: [true, false], benefits: [false, true] },
+  { name: "Olive Oil", description: "Actually useful", uses: [true, false], benefits: [false, true] }
+]
