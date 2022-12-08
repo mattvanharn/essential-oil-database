cdffe943f5598fb0bba474791e7e594a806c3806
David-Heynen <heynendavid@gmail.com>
2022-12-08 14:09:49 -0500

    product-card

--- essentialOils/src/app/app.component.ts
+++ essentialOils/src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
+interface essentialOilsData {
+  name: string;
+  description: string;
+  uses: [];
+  benefits: [];
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'essentialOils';
+  public productName = '';
+  public productDescription = '';
+  public productUses = {};
+  public productBenefits = {};
+
+  constructor(private db: AngularFirestore) {
+    db.firestore.settings({ experimentalForceLongPolling: true });
+    db.doc<essentialOilsData>('/products/clarysage')
+      .valueChanges()
+      .subscribe((result) => {
+        if (result) {
+          console.log(result.benefits);
+          this.productName = result.name;
+          this.productDescription = result.description;
+          db.doc('/products/clarysage/uses/usesDoc')
+            .valueChanges()
+            .subscribe((res) => {
+              if (res) {
+                console.log(res);
+              }
+            });
+        }
+      });
+  }
 }
--- essentialOils/src/app/app.module.ts
+++ essentialOils/src/app/app.module.ts
@@ -2,15 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ProductCardComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatCardModule,
+    MatIconModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
