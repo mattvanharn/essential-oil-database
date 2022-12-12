406ebded54f1d1beab833a4c9d3f096d2382bb02
David-Heynen <85192794+David-Heynen@users.noreply.github.com>
2022-12-12 16:16:23 -0500

    Revert "implemented firestore"

--- essentialOils/src/app/display-product/display-product.component.ts
+++ essentialOils/src/app/display-product/display-product.component.ts
@@ -1,67 +1,23 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Product } from '../product';
 import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-display-product',
   templateUrl: './display-product.component.html',
-  styleUrls: ['./display-product.component.scss'],
+  styleUrls: ['./display-product.component.scss']
 })
 export class DisplayProductComponent implements OnInit {
   products: Product[] = [];
 
-  constructor(
-    private productService: ProductService,
-    private db: AngularFirestore
-  ) {
-    db.firestore.settings({ experimentalForceLongPolling: true });
-    db.collection<Product>('products')
-      .valueChanges()
-      .subscribe((items) => {
-        if (items) {
-          this.products = [];
-          items.forEach((doc) => {
-            this.products.push(doc);
-          });
-        }
-        console.log(this.products[0].uses);
-      });
-  }
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
     this.getProducts();
   }
 
   getProducts(): void {
-    //this.products = this.productService.getProducts();
+    this.products = this.productService.getProducts();
   }
 
-  deleteProduct(product: Product): void {
-    this.db
-      .doc('/products/' + product.name.replace(/\s/g, '').toLowerCase())
-      .delete()
-      .then(() => {
-        console.log('Document successfully deleted!');
-      })
-      .catch((err) => {
-        console.error('Error removing doc: ', err);
-      });
-    this.updateData();
-  }
-
-  updateData() {
-    this.db
-      .collection<Product>('products')
-      .valueChanges()
-      .subscribe((items) => {
-        if (items) {
-          this.products = [];
-          items.forEach((doc) => {
-            this.products.push(doc);
-          });
-        }
-        console.log(this.products[0].uses);
-      });
-  }
 }
