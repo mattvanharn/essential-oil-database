4ffd2be802908960cc8925e5f6f897f0f68a7d9b
Matt <102394946+mattvanharn@users.noreply.github.com>
2022-12-13 15:21:21 -0500

    Move data collection to service component, add Observables (#11)

--- essentialOils/src/app/app.component.ts
+++ essentialOils/src/app/app.component.ts
@@ -4,10 +4,10 @@ import { Form, FormBuilder, FormControl } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { AddProductComponent } from './add-product/add-product.component';
 
-import { PRODUCTS } from './mock-products';
 import { ProductService } from './product.service';
 import { Product } from './product';
 import { NgOptimizedImage } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -15,8 +15,6 @@ import { NgOptimizedImage } from '@angular/common';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  products: Product[] = [];
-  displayedProducts: Product[] = [];
 
   checked1: boolean = false;
   checked2: boolean = false;
@@ -28,37 +26,28 @@ export class AppComponent implements OnInit {
   checked8: boolean = false;
   checked9: boolean = false;
 
-  constructor(
-    private dialogRef: MatDialog,
-    public productService: ProductService,
-    private _formBuilder: FormBuilder,
-    private db: AngularFirestore
-  ) {}
+  products$!: Observable<Product[]>;
+  displayedProducts$!: Observable<Product[]>;
+
+  constructor(private dialogRef: MatDialog, public productService: ProductService, private _formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
-    this.db.firestore.settings({ experimentalForceLongPolling: true });
-    this.db
-      .collection<Product>('products')
-      .valueChanges()
-      .subscribe((items) => {
-        if (items) {
-          this.products = [];
-          items.forEach((doc) => {
-            this.products.push(doc);
-          });
-        }
-        console.log(this.products[0].uses);
-      });
-    this.displayedProducts = this.products;
+    this.getProducts$();
   }
-
-  filterProductsByUse(index: number) {
-    return this.products.filter((product) => product.uses[index] === true);
+  
+  getProducts$() {
+    this.products$ = this.productService.getProducts$();
+    this.displayedProducts$ = this.products$;
     throw new Error('Method not implemented.');
   }
 
-  filterProductsByBenefit(index: number) {
-    return this.products.filter((product) => product.benefits[index] === true);
+  filterProducts$ByUse(index: number) {
+    // return (this.products$).filter(product$ => product$.uses[index] === true);
+    throw new Error('Method not implemented.');
+  }
+
+  filterProducts$ByBenefit(index: number) {
+    // return (this.products$).filter(product$ => product$.benefits[index] === true);
     throw new Error('Method not implemented.');
   }
 
@@ -75,89 +64,71 @@ export class AppComponent implements OnInit {
 
   onChecked1() {
     this.checked1 = !this.checked1;
-    this.checked1
-      ? (this.displayedProducts = this.filterProductsByUse(0))
-      : (this.displayedProducts = this.products);
+    // this.checked1 ? this.displayedProducts$ = this.filterProducts$ByUse(0) : this.displayedProducts$ = this.products$;
     return false;
     throw new Error('Method not implemented.');
   }
 
   onChecked2() {
     this.checked2 = !this.checked2;
-    this.checked2
-      ? (this.displayedProducts = this.filterProductsByUse(1))
-      : (this.displayedProducts = this.products);
-    console.table(this.displayedProducts);
+    // this.checked2 ? this.displayedProducts$ = this.filterProducts$ByUse(1) : this.displayedProducts$ = this.products$;
+    console.table(this.displayedProducts$);
     return false;
     throw new Error('Method not implemented.');
   }
 
   onChecked3() {
     this.checked3 = !this.checked3;
-    this.checked3
-      ? (this.displayedProducts = this.filterProductsByUse(2))
-      : (this.displayedProducts = this.products);
-    console.table(this.displayedProducts);
+    // this.checked3 ? this.displayedProducts$ = this.filterProducts$ByUse(2) : this.displayedProducts$ = this.products$;
+    console.table(this.displayedProducts$);
     return false;
     throw new Error('Method not implemented.');
   }
 
   onChecked4() {
     this.checked4 = !this.checked4;
-    this.checked4
-      ? (this.displayedProducts = this.filterProductsByUse(3))
-      : (this.displayedProducts = this.products);
-    console.table(this.displayedProducts);
+    // this.checked4 ? this.displayedProducts$ = this.filterProducts$ByUse(3) : this.displayedProducts$ = this.products$;
+    console.table(this.displayedProducts$);
     return false;
     throw new Error('Method not implemented.');
   }
 
   onChecked5() {
     this.checked5 = !this.checked5;
-    this.checked5
-      ? (this.displayedProducts = this.filterProductsByBenefit(0))
-      : (this.displayedProducts = this.products);
-    console.table(this.displayedProducts);
+    // this.checked5 ? this.displayedProducts$ = this.filterProducts$ByBenefit(0) : this.displayedProducts$ = this.products$;
+    console.table(this.displayedProducts$);
     return false;
     throw new Error('Method not implemented.');
   }
 
   onChecked6() {
     this.checked6 = !this.checked6;
-    this.checked6
-      ? (this.displayedProducts = this.filterProductsByBenefit(1))
-      : (this.displayedProducts = this.products);
-    console.table(this.displayedProducts);
+    // this.checked6 ? this.displayedProducts$ = this.filterProducts$ByBenefit(1) : this.displayedProducts$ = this.products$;
+    console.table(this.displayedProducts$);
     return false;
     throw new Error('Method not implemented.');
   }
 
   onChecked7() {
     this.checked7 = !this.checked7;
-    this.checked7
-      ? (this.displayedProducts = this.filterProductsByBenefit(2))
-      : (this.displayedProducts = this.products);
-    console.table(this.displayedProducts);
+    // this.checked7 ? this.displayedProducts$ = this.filterProducts$ByBenefit(2) : this.displayedProducts$ = this.products$;
+    console.table(this.displayedProducts$);
     return false;
     throw new Error('Method not implemented.');
   }
 
   onChecked8() {
     this.checked8 = !this.checked8;
-    this.checked8
-      ? (this.displayedProducts = this.filterProductsByBenefit(3))
-      : (this.displayedProducts = this.products);
-    console.table(this.displayedProducts);
+    // this.checked8 ? this.displayedProducts$ = this.filterProducts$ByBenefit(3) : this.displayedProducts$ = this.products$;
+    console.table(this.displayedProducts$);
     return false;
     throw new Error('Method not implemented.');
   }
 
   onChecked9() {
     this.checked9 = !this.checked9;
-    this.checked9
-      ? (this.displayedProducts = this.filterProductsByBenefit(4))
-      : (this.displayedProducts = this.products);
-    console.table(this.displayedProducts);
+    // this.checked9 ? this.displayedProducts$ = this.filterProducts$ByBenefit(4) : this.displayedProducts$ = this.products$;
+    console.table(this.displayedProducts$);
     return false;
     throw new Error('Method not implemented.');
   }
@@ -170,10 +141,10 @@ export class AppComponent implements OnInit {
   });
 
   benefits = this._formBuilder.group({
-    boostMood: true,
+    boostMood: false,
     inflammation: false,
-    painRelief: true,
-    sleep: true,
+    painRelief: false,
+    sleep: false,
     stressRelief: false,
   });
 }
--- essentialOils/src/app/display-product/display-product.component.ts
+++ essentialOils/src/app/display-product/display-product.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable, Subject } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { EditProductComponent } from '../edit-product/edit-product.component';
+
 import { Product } from '../product';
 import { ProductService } from '../product.service';
 
@@ -10,24 +12,28 @@ import { ProductService } from '../product.service';
   templateUrl: './display-product.component.html',
   styleUrls: ['./display-product.component.scss'],
 })
-export class DisplayProductComponent {
-  products: Product[] = [];
 
-  constructor(private dialogRef: MatDialog, private db: AngularFirestore) {
-    db.firestore.settings({ experimentalForceLongPolling: true });
-    db.collection<Product>('products')
-      .valueChanges()
-      .subscribe((items) => {
-        if (items) {
-          this.products = [];
-          items.forEach((doc) => {
-            this.products.push(doc);
-          });
-        }
-        console.log(this.products[0].uses);
-      });
+export class DisplayProductComponent implements OnInit {
+
+  products$!: Observable<Product[]>;
+  displayedProducts!: Observable<Product[]>;
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.getProducts$();
   }
 
+  getProducts$() {
+    this.products$ = this.productService.getProducts$();
+    this.displayedProducts = this.products$;
+    throw new Error('Method not implemented.');
+  }
+
+  deleteProduct(product: Product): void {
+    this.productService.deleteProduct(product);
+    }
+    
   openEditDialog(product: Product) {
     this.dialogRef.open(EditProductComponent, {
       data: {
@@ -37,17 +43,5 @@ export class DisplayProductComponent {
         benefits: product.benefits,
       },
     });
-  }
-
-  deleteProduct(product: Product): void {
-    this.db
-      .doc('/products/' + product.name.replace(/\s/g, '').toLowerCase())
-      .delete()
-      .then(() => {
-        console.log('Document successfully deleted!');
-      })
-      .catch((err) => {
-        console.error('Error removing doc: ', err);
-      });
   }
 }
